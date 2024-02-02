84e5a650d7bc82221e1f9140dd157493759287aa
palshe <151994170+palshe@users.noreply.github.com>
2024-02-02 12:55:41 +0000

    finish 14.2.4

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -29,4 +29,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?]", logout_path
   end 
+
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
+  end
 end
